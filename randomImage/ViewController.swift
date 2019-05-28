d4c28c55cff38f3502aa5b688c1b857138f31a03
onemoon <onemoonstudio@gmail.com>
2019-05-28 23:45:39 +0900

    feat: Base

---
+++ randomImage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  randomImage
+//
+//  Created by Hyeontae on 28/05/2019.
+//  Copyright © 2019 onemoonStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
